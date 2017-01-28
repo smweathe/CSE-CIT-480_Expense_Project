aa801afed8eaf86e52a407a27dfe22050f6a73a3
Boban Ljuljdjurovic <ljuljdjurovic@gmail.com>
2017-01-28 15:05:02 +0000

    Clean Up

--- signup.php
+++ signup.php
@@ -7,9 +7,9 @@
 	$url = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
 	//notify user of missing field/s
 	if (strpos($url,'error=empty')!==false){
-		echo "Fill all fields to sign up.";
+		echo "<br><center><label>Fill all fields to sign up.</label></center><br<br>";
 	}elseif (strpos($url,'error=username')!==false){
-		echo "Username already exists. Please choose another.";
+		echo "<br><center><label>Username is already taken. Please try another one!</label></center><br<br>";
 	}
 	if (isset($_SESSION['id'])){
 		//prevent user from signing up while logged in
@@ -28,16 +28,16 @@ $(document).ready(function(){
 <html>
 <body>
 <form action='includes/signup.inc.php' method='POST'>
-	<center><label>Sign Up Screen</label><br></center><br><br>
+	<br><center><label>Sign Up Screen</label><br></center><br><br>
 	<center><label>Please fill out all forms below to join our site.</label><br></center><br><br>
 	<center><input type='text' name='first' placeholder='Firstname'><br><br></center>
 	<center><input type='text' name='last' placeholder='Lastname'><br><br></center>
 	<center><input type='text' name='uid' placeholder='Username'><br><br></center>
-	<center><input type='password' name='pwd' placeholder='Password'><br><br></center>
+	<center><input type='text' name='email' placeholder='Email'><br><br></center>
+	<center><input type='password' name='pwd' placeholder='Password'><br><br></center>	
 	<center><label id="sup"><input type="checkbox" name="isSupervisor" id="checkbox">Are you a supervisor?</label><br><br></center>
 	<center><input type='text' name='supervisorName' placeholder='Supervisor Name' id='sName'><br><br></center>
 	<center><button type='submit'>Sign Up</button><br></center>
 </form>
-<button id='butt'>Disappear</button>
 </body>
 </html>
