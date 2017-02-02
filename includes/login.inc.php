6c4cd0ff0095531460c162982605cdf199ef1180
Stephen Weatherly <weatherly.steve@gmail.com>
2017-02-01 21:31:57 -0500

    Update login.inc.php

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -1,17 +1,25 @@
 <?php
 session_start();
 include '../dbh.php';
+
 $uid=$_POST['uid'];
 $pwd=$_POST['pwd'];
 
-$sql = "SELECT * FROM user WHERE uid='$uid' AND pwd='$pwd'";
-$result= mysqli_query($conn,$sql);
-
-//no db results
-if(!$row=mysqli_fetch_assoc($result)){
-	echo "Your username or password is incorrect";
+if (empty($uid)){
+	header("Location: ../login.php?error=empty");
+	exit();
+	}
+if (empty($pwd)){
+	header("Location: ../login.php?error=empty");
+	exit();
 }else{
-	//setup session for user
-	$_SESSION['id'] = $row['id'];
+	$sql = "SELECT * FROM user WHERE uid='$uid' AND pwd='$pwd'";
+	$result= mysqli_query($conn,$sql);
+
+	if(!$row=mysqli_fetch_assoc($result)){
+		echo "Your username or password is incorrect";
+	}else{
+		$_SESSION['id'] = $row['id'];
+	}
+	header("Location: ../account.php");
 }
-header("Location: ../index.php");
