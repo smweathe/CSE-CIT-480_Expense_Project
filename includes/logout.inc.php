606cb9b7fe548cd9d01f711411bb2e8f88dde353
Stephen Weatherly <weatherly.steve@gmail.com>
2017-02-06 16:36:36 -0500

    Update logout.inc.php

--- includes/logout.inc.php
+++ includes/logout.inc.php
@@ -1,7 +1,4 @@
 <?php
-    session_start();
-    session_unset();
-    session_destroy();
-    header('Location: http://localhost/ExpenseMaster/index.php');
-    exit;
-?>
+session_start();
+session_destroy();
+header("Location: ../index.php");
