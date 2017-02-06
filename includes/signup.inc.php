b0e3c1e8914d779700ab817b722f2e5eb1957270
Stephen Weatherly <weatherly.steve@gmail.com>
2017-02-06 16:37:11 -0500

    Update signup.inc.php

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -5,38 +5,45 @@ include '../dbh.php';
 $first=$_POST['first'];
 $last=$_POST['last'];
 $email=$_POST['email'];
+$bid=$_POST['bid'];
 $uid=$_POST['uid'];
 $pwd=$_POST['pwd'];
 
 if (empty($first)){
-	header("Location: ../signup.php?error=empty");
+	header("Location: ../index.php?error=empty");
 	exit();
 	}
 if (empty($last)){
-	header("Location: ../signup.php?error=empty");
+	header("Location: ../index.php?error=empty");
 	exit();
 	}
 if (empty($email)){
-	header("Location: ../signup.php?error=empty");
+	header("Location: ../index.php?error=empty");
+	exit();
+	}
+if (empty($bid)){
+	header("Location: ../index.php?error=empty");
 	exit();
 	}
 if (empty($uid)){
-	header("Location: ../signup.php?error=empty");
+	header("Location: ../index.php?error=empty");
 	exit();
 	}
 if (empty($pwd)){
-	header("Location: ../signup.php?error=empty");
+	header("Location: ../index.php?error=empty");
 	exit();
 	} else {
 		$sql = "SELECT uid FROM user WHERE uid='$uid'";
 		$result= mysqli_query($conn,$sql);
 		$uidcheck= mysqli_num_rows($result);
 
-		if ($uidcheck >0){
-			header("Location: ../signup.php?error=username");
+		if ($uidcheck >=1){
+			echo '<script type="text/javascript">alert("Username already exists.");
+			window.location.href = "http://localhost/ExpenseMaster/index.php"
+			</script>';
 			exit();
 		} else {
-			$sql="INSERT INTO user (first,last,email,uid,pwd) VALUES('$first','$last','$email','$uid','$pwd')";
+			$sql="INSERT INTO user (first,last,email,bid,uid,pwd) VALUES('$first','$last','$email','$bid','$uid','$pwd')";
 			$result= mysqli_query($conn,$sql);
 
 			header("Location: ../confirm.php");
