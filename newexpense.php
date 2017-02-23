05ccd623e378c8ee73aa2c0712d9eb5163f0c077
Stephen Weatherly <weatherly.steve@gmail.com>
2017-02-22 19:51:34 -0500

    Create newexpense.php

---
+++ newexpense.php
@@ -0,0 +1,62 @@
+<?php
+    include 'dbh.php';
+  	session_start();
+    /*prevent user from accessing this page
+    if no session is started*/
+    if (!isset($_SESSION['id'])){
+        header("Location: login.php");
+  }
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-type" />
+    <title>Expense Master | Sign up</title>
+    <link href="style.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/44ff956945.css">
+	<!-- Favorite Icon -->
+	<link rel="shortcut icon" href="./images/icon.png" type="image/x-icon" />
+    <meta content="width=device-width, initial-scale=1" name="viewport" />
+    <meta content="Sign up for a free Expense Master account." name="description" />
+    <!-- Logo Redirect to a page at # -->
+	<link href="./index.php" rel="canonical" />
+  </head>
+  <body class="signup">
+  <noscript><iframe height="0" src="//www.googletagmanager.com/ns.html?id=GTM-JD26" style="display:none;visibility:hidden" width="0"></iframe></noscript>
+    <div class="signup-page">
+
+      <div class="header-main">
+        <a class="header-logo" href="./account.php"></a>
+        <div class="header-login">
+    			<!-- Login Redirect to a page at # 
+    			<a class="btn btn-sm btn-alt" href="#">Log in</a>
+    			 Logout Redirect to a page at # -->
+    			<a class="btn btn-sm btn-alt" href="./logout.php"><i class="fa fa-sign-out" aria-hidden="true"></i>&nbsp; Log Out</a>
+        </div>
+      </div>
+    <div class="signup-content" style="padding: 0px;">
+          <ul class="side-by-side">
+                <li><a class="active"  href="./account.php"><i class="fa fa-home" aria-hidden="true"></i>&nbsp; My Account</a></li>
+                <!--Modal form will pop up to submit a new expense-->
+                <li><a href="./newexpense.php"><i class="fa fa-plus" aria-hidden="true"></i>&nbsp; New Expense</a></li>
+                <li><a href="./receipt.php"><i class="fa fa-cloud-upload" aria-hidden="true"></i>&nbsp; Upload Receipt</a></li>
+                <li><a href="./report.php"><i class="fa fa-file-text" aria-hidden="true"></i>&nbsp; Run Report</a></li>
+          </ul>
+    <div class="benefits page-body" style="padding: 30px;">
+          <div class="list-group" style="padding:30px; background:#F9F9FB; border:1px solid #E4E4E7; border-radius:5px;">
+            <h3>Please choose an expense type to begin.</h3>
+            <a class="btn btn-sm btn-alt" href="./generalexpenseform.php"><i class="fa fa-money fa-fw" aria-hidden="true"></i>&nbsp; General expense</a></br>
+            <p>Select this option for items such as entertainment, lodging, and meals.</p>
+            <a class="btn btn-sm btn-alt" href="./distanceform.php"><i class="fa fa-plane" aria-hidden="true"></i>&nbsp; Distance</a></br>
+            <p>Select this option for travel via plane, train, or car.</p>
+            <a class="btn btn-sm btn-alt" href="./timeform.php"><i class="fa fa-clock-o" aria-hidden="true"></i>&nbsp; Time</a></br>
+            <p>Select this option for hourly compensation.</p>
+          </div>
+    </div>		
+    <div class="signup page-sidebar"><p class="alert alert-error" id="fill-all-fields" style="display:none"></p>
+		</div>
+      </div>
+    </div><script src="//d2fjue5z6foteq.cloudfront.net/assets/315cc4a6724c52ae0b7b8f0104132a7094855698/main.js" type="text/javascript"></script>
+  </body>
+</html>
