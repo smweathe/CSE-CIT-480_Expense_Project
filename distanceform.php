9e5f78d4f5e821be246fc35921a59753d0d50255
Stephen Weatherly <weatherly.steve@gmail.com>
2017-03-12 20:58:15 -0400

    Update distanceform.php
    Minor UI overhaul, added auto totaling based on distance and rate, validation that all required textboxes are filled in the correct form.

--- distanceform.php
+++ distanceform.php
@@ -11,10 +11,13 @@
 <!DOCTYPE html>
 <html>
   <head>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <meta content="text/html; charset=utf-8" http-equiv="Content-type" />
-    <title>Expense Master | Sign up</title>
-    <link href="style.css" rel="stylesheet" />
+    <title>ExpenseMaster - Distance</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/44ff956945.css">
+    <link href="w3.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet" />
   <!-- Favorite Icon -->
   <link rel="shortcut icon" href="./images/icon.png" type="image/x-icon" />
     <meta content="width=device-width, initial-scale=1" name="viewport" />
@@ -44,23 +47,51 @@
                 <li><a href="./report.php"><i class="fa fa-file-text" aria-hidden="true"></i>&nbsp; Run Report</a></li>
           </ul>
     <div class="benefits page-body" style="padding: 30px;">
+    <h2>Fill Out a New Car Distance Expense</h2>
           <form action="#" class="gtm_expense_form" id="expenseform" method="post">
             <div class="input-group">
-            <h3><center>Distance Form</center></h3>
-            <center><i class="fa fa-plane fa-5x" aria-hidden="true"></i></center>
+            <center><i class="fa fa-car fa-5x" aria-hidden="true"></i></center>
             <br>
-            Distance($0.54 per mile): <input type="number" class="required" name="distance" placeholder="miles" tabindex="1" min="0" max="9999999.99" step"1" style="margin-bottom: 5px"><sup class="required" title="Required"></sup>
-            Date: <input type="date" class="required" name="date"  tabindex="2" style="margin-bottom: 5px"><sup class="required" title="Required"></sup>
-            Total:<input type="number" class="required" name="total" placeholder="$0.00" tabindex="3" min="0.00" max="9999999.99" step"1" style="margin-bottom: 5px"><sup class="required" title="Required"></sup>
-            Additional Comments: <input type="text" name="comment" tabindex="4" style="margin-bottom: 5px"><sup class="required" title="Required"></sup>
-            Upload Receipt: <input type="file" name="expense_receipt" tabindex="5" style="margin-bottom: 5px"><br><br>
+            Distance(in miles): <input type="text" class="required" name="distance" id="distance" required value="" autocomplete="false" value="" tabindex="1" style="margin-bottom: 5px"><sup class="required" title="Required"></sup>
+            Rate(0.54 is default): <input type="text"  class="required" name="rate"  id="rate" required value="" autocomplete="false" value="" onclick= "calc()" tabindex="2"  style="margin-bottom: 5px"><sup class="required" title="Required"></sup>
+            Total: <input type="text" class="required" name="total" id="total" value="" placeholder="$0.00" tabindex="3" disabled="disabled" style="margin-bottom: 5px; background-color: #f3f5f6;"><sup class="required" title="Required"></sup>
+            
+            <script>
+            //User can only enter numbers and decimal
+              $(document).ready(function () {
+                //called when key is pressed in textbox
+                $("#distance,#rate").keypress(function (e) {
+                   //if the letter is not digit then display error and don't type anything
+                   if (e.which != 46 && e.which > 31 && (e.which < 48 || e.which > 57)) {
+                      //display error message
+                      $("#errmsg").html("Digits Only").show().fadeOut("slow");
+                             return false;
+                  }
+                 });
+              });
+
+            //Calculate Total
+            function calc(){
+                $("#distance,#rate").keyup(function () {
+                $('#total').val($('#distance').val() * $('#rate').val());
+                });
+            }
+            </script>
+
+            Date: <input type="date" class="required" name="date" required value=""  tabindex="4" style="margin-bottom: 5px"><sup class="required" title="Required"></sup>
+            Additional Comments: <textarea name="comment" rows="5" tabindex="5" style="margin-bottom: 5px"></textarea><sup class="required" title="Required"></sup>
+            Upload Receipt: <input type="file" name="expense_receipt" tabindex="6" style="margin-bottom: 5px"><br><br>
             <input class="btn btn-alt full-width" href="./account.php" tabindex="7" type="submit" value="Submit Form" />
             <br><br>
          </form>
-    </div>    
-    <div class="signup page-sidebar">
-    </div>
+    </div>   
       </div>
+
+    <div class="signup page-sidebar">
+            <h3>This form can be used to fill out a new car expense. The default mileage rate is $0.54 per mile.</h3>
+            <h3>Please Note: Comments and receipt upload are not required for submission.</h3>
+    </div>
+
     </div><script src="//d2fjue5z6foteq.cloudfront.net/assets/315cc4a6724c52ae0b7b8f0104132a7094855698/main.js" type="text/javascript"></script>
   </body>
 </html>
