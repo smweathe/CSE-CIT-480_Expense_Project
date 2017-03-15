d7e28f5bf5e9433e35feb1e074ee25f36faefe23
Stephen Weatherly <weatherly.steve@gmail.com>
2017-03-15 18:03:30 -0400

    Update index.php
    Added Birthday Field, changed user input verification, and added footer functionality.

--- index.php
+++ index.php
@@ -38,20 +38,12 @@
       </div>
 
 <!--Left Side-->
-      <div class="signup-content">
+      <div class="signup-content" style="background-image: url(./images/world.jpg);">
         <div class="benefits page-body">
-          <h1 style="margin: 0 0 20px 0;" align="center">Welcome to ExpenseMaster</h1>
+          <h1 style="margin: 0 0 20px 0; font-weight: bold;" align="center">Welcome to ExpenseMaster</h1>
           <h5 style="margin: 0 0 20px 0;" align="center">"Simplying Company Expense Tracking"</h5>
-            <div class="welcome1" style="background:#F9F9FB; padding: 10px; border:1px solid #E4E4E7; border-radius:5px;">
-            <h3>Mission Statement</h3>
-            <p>Our goal is to improve the efficiency and accuracy of all company expense tracking.
-              For companies still using spreadsheet we offer .csv uploads and convert them to online forms.
-              We are a cloud based solution for companies who need to track company spending.
-              Joining today gives you access to all our tools and analytics for tracking company expenses.</p>
-              <br><br>
-              <p align="center"><b>Join now and see what you have been missing!</b></p>
-              <br>
-              <h6 style="margin: 0!important; text-align: center;">-The ExpenseMaster team</h6>
+            <div class="welcome1" style="">
+            <img src="./images/rsz_business.png" alt="Team Meeting" style="width:100%; border-radius:5px; max-width: 100%;height: auto;">
             </div>
             <br>
             <div class="welcome2" style="background:#F9F9FB; padding: 10px; border:1px solid #E4E4E7; border-radius:5px;">
@@ -65,19 +57,7 @@
               <br>
               <h6 style="margin: 0!important; text-align: center;">-The ExpenseMaster team</h6>
             </div>
-            <br>
-            <div class="welcome3" style="background:#F9F9FB; padding: 10px; border:1px solid #E4E4E7; border-radius:5px;">
-            <h3>Mission Statement</h3>
-            <p>Our goal is to improve the efficiency and accuracy of all company expense tracking.
-              For companies still using spreadsheet we offer .csv uploads and convert them to online forms.
-              We are a cloud based solution for companies who need to track company spending.
-              Joining today gives you access to all our tools and analytics for tracking company expenses.</p>
-              <br><br>
-              <p align="center"><b>Join now and see what you have been missing!</b></p>
-              <br>
-              <h6 style="margin: 0!important; text-align: center;">-The ExpenseMaster team</h6>
-            </div>
-            <br>
+            <br><br>
         </div>
 
 <!--Right Side-->
@@ -89,19 +69,90 @@
               <h3 align="center" class="join" style="background-color: #2E3639; padding: 0px; font-size: 24px;color: #fff; padding-left: 5px;" >Join Now</h3><i class="fa fa-pencil fa-2x" style="color:white" aria-hidden="true"></i><br>
             </div>
 
-    				<div class="input-group">
-              <p>Please fill out the form below to create an account and join our thriving community. Signing up is always free, so join today to unlock all of our member benefits.</p>
-              <br> 
-              <p>If you have any difficulty signing up, please email our support team at <a style="text-decoration: none; font-weight: bold;" href="mailto:support@expensemaster.com">support@expensemaster.com</a>.</p>
+<!--Registration Form-->
+    				<div class="input-group" id="input-group">
+              <p>If you have any difficulty signing up, please email our support team at <a style="text-decoration: none; font-weight: bold; color: darkred;" href="mailto:support@expensemaster.com">support@expensemaster.com</a>.</p>
               <br>
-    				  <input type="text" class="required" name="first" placeholder="First name" tabindex="1" style="margin-bottom: 5px"><sup class="required" title="Required"></sup>
-    				  <input type="text" class="required" name="last" placeholder="Last name" tabindex="2" style="margin-bottom: 5px"><sup class="required" title="Required"></sup>
-    				  <input type="email" class="required" name="email" placeholder="email" tabindex="3" style="margin-bottom: 5px"><sup class="required" title="Required"></sup>
-              <input type="text" class="required" name="bid" placeholder="business id" tabindex="4" style="margin-bottom: 5px"><sup class="required" title="Required"></sup>
-              <input type="text" class="required" name="uid" placeholder="username" tabindex="4" style="margin-bottom: 5px"><sup class="required" title="Required"></sup>
-    				  <input type="password" class="required" name="pwd" placeholder="password" tabindex="5" style="margin-bottom: 5px"><sup class="required" title="Required"></sup>
+              <strong><label style="margin-top:.3em; color: black;">Name</label></strong>
+    				  <input type="text" required="" name="first" placeholder="First" tabindex="1" style="margin-bottom: 5px; width: 49%; margin-right:3px;"><input type="text" required="" name="last" placeholder="Last" tabindex="2" style="margin-bottom: 5px; width: 49%; float: right;">
+              <strong><label style="margin-top:.3em; color: black;">Email</label></strong>
+    				  <input type="email" required="" name="email" placeholder="" tabindex="2" style="margin-bottom: 5px">
+              <strong><label style="margin-top:.3em; color: black;">Business</label></strong>
+              <input type="text" required="" name="bid" placeholder="" tabindex="3" style="margin-bottom: 5px">
+              <strong><label style="margin-top:.3em; color: black;">Date of Birth</label></strong>
+              <div class="dob" style="width: 100%; height: 40px; margin-bottom: 5px; display: inline-block;">
+                  <select required name="month" id="month" style="width: 32%;" tabindex="4" onChange="changeDate(this.options[selectedIndex].value);">
+                      <option value="">Month</option>
+                      <option value="1">January</option>
+                      <option value="2">February</option>
+                      <option value="3">March</option>
+                      <option value="4">April</option>
+                      <option value="5">May</option>
+                      <option value="6">June</option>
+                      <option value="7">July</option>
+                      <option value="8">August</option>
+                      <option value="9">September</option>
+                      <option value="10">October</option>
+                      <option value="11">November</option>
+                      <option value="12">December</option>
+                  </select><select name="day" id="day" required="" style="width: 32%;  margin-right:2%; margin-left: 2%;" tabindex="5" id="day">
+                      <option value="">Day</option>
+                  </select><select name="year" id="year" required="" style="width: 32%;" tabindex="6" id="year">
+                      <option value="">Year</option>
+                  </select>
+              </div>
+
+<!--Determine number of days based on Month Selected-->
+              <script language="JavaScript" type="text/javascript">
+                function changeDate(i){
+                  var e = document.getElementById('day');
+                  while(e.length>0)
+                  e.remove(e.length-1);
+                  var j=-1;
+                  if(i=="")
+                    k=0;
+                  else if(i==2)
+                    k=28;
+                  else if(i==4||i==6||i==9||i==11)
+                    k=30;
+                  else
+                    k=31;
+                  while(j++<k){
+                    var s=document.createElement('option');
+                    var e=document.getElementById('day');
+                    if(j==0){
+                      s.text="Day";
+                      s.value="";
+                    try{
+                      e.add(s,null);}
+                    catch(ex){
+                      e.add(s);}
+                    }else{
+                      s.text=j;
+                      s.value=j;
+                      try{
+                        e.add(s,null);}
+                      catch(ex){
+                        e.add(s);}}}}
+                    y = 1997;
+                    while (y-->1917){
+                      var s = document.createElement('option');
+                      var e = document.getElementById('year');
+                      s.text=y;
+                      s.value=y;
+                      try{
+                        e.add(s,null);}
+                      catch(ex){
+                        e.add(s);}}
+                </script>
+              <strong><label style="margin-top:.3em; color: black;">Choose a username</label></strong>
+              <input type="text" required="" name="uid" placeholder="" tabindex="7" style="margin-bottom: 5px">
+              <strong><label style="margin-top:.3em; color: black;">Create a password</label></strong>
+    				  <input type="password" required="" name="pwd" placeholder="" tabindex="8" style="margin-bottom: 5px">
     				  <br><br>
-    				  <input class="btn btn-alt full-width" href="./confirm.php" disabled="disabled" tabindex="7" type="submit" value="Create Account">
+              <p>By creating an account you agree to our <a href="#" style="color: blue">Terms & Privacy</a>.</p>
+              <br>
+    				  <input class="btn btn-alt full-width" href="./confirm.php" disabled="disabled" tabindex="9" type="submit" value="Create Account">
     				</div>
 			   </form>
 		    </div>
@@ -119,7 +170,9 @@
         <!--<img src="/w3images/avatar.jpg" alt="Boss" style="width:45%" class="w3-circle w3-hover-opacity">-->
         <h4 style="margin: 10px">Stephen Weatherly</h4>
         <p>Web Designer/Lead Programmer</p>
-        <br>
+        <a href="javascript:void(0)" title="Linkedin"><i class="fa fa-linkedin fa-2x" style="padding: 0px 5px 0px 5px;color: #0077B5"></i></a>
+        <a href="javascript:void(0)" title="Linkedin"><i class="fa fa-github fa-2x" style="padding: 0px 5px 0px 5px;"></i></a>
+        <br><br>
       </div>
 
       <div class="w3-quarter">
@@ -127,7 +180,9 @@
         <!--<img src="/w3images/avatar.jpg" alt="Boss" style="width:45%" class="w3-circle w3-hover-opacity">-->
         <h4 style="margin: 10px">Boban Ljuljdjurovic </h4>
         <p>Lead Programmer</p>
-        <br>
+        <a href="javascript:void(0)" title="Linkedin"><i class="fa fa-linkedin fa-2x" style="padding: 0px 5px 0px 5px;color: #0077B5"></i></a>
+        <a href="javascript:void(0)" title="Linkedin"><i class="fa fa-github fa-2x" style="padding: 0px 5px 0px 5px;"></i></a>
+        <br><br>
       </div>
 
       <div class="w3-quarter">
@@ -135,7 +190,9 @@
         <!--<img src="/w3images/avatar.jpg" alt="Boss" style="width:45%" class="w3-circle w3-hover-opacity">-->
         <h4 style="margin: 10px">Eric Cicci</h4>
         <p>Project Manager</p>
-        <br>
+        <a href="javascript:void(0)" title="Linkedin"><i class="fa fa-linkedin fa-2x" style="padding: 0px 5px 0px 5px;color: #0077B5"></i></a>
+        <a href="javascript:void(0)" title="Linkedin"><i class="fa fa-github fa-2x" style="padding: 0px 5px 0px 5px;"></i></a>
+        <br><br>
       </div>
 
       <div class="w3-quarter">
@@ -143,7 +200,9 @@
         <!--<img src="/w3images/avatar.jpg" alt="Boss" style="width:45%" class="w3-circle w3-hover-opacity">-->
         <h4 style="margin: 10px">Alex Tunis</h4>
         <p>Datbase Designer/App Developer</p>
-        <br>
+        <a href="javascript:void(0)" title="Linkedin"><i class="fa fa-linkedin fa-2x" style="padding: 0px 5px 0px 5px;color: #0077B5"></i></a>
+        <a href="javascript:void(0)" title="Linkedin"><i class="fa fa-github fa-2x" style="padding: 0px 5px 0px 5px;"></i></a>
+        <br><br>
       </div>
 
       <div class="w3-quarter">
@@ -151,7 +210,9 @@
         <!--<img src="/w3images/avatar.jpg" alt="Boss" style="width:45%" class="w3-circle w3-hover-opacity">-->
         <h4 style="margin: 10px">Sean Ruta</h4>
         <p>Documentation</p>
-        <br>
+        <a href="javascript:void(0)" title="Linkedin"><i class="fa fa-linkedin fa-2x" style="padding: 0px 5px 0px 5px; color: #0077B5;"></i></a>
+        <a href="javascript:void(0)" title="Linkedin"><i class="fa fa-github fa-2x" style="padding: 0px 5px 0px 5px;"></i></a>
+        <br><br>
       </div>
 
       <div class="w3-quarter">
@@ -159,7 +220,9 @@
         <!--<img src="/w3images/avatar.jpg" alt="Boss" style="width:45%" class="w3-circle w3-hover-opacity">-->
         <h4 style="margin: 10px">Peter Lutz</h4>
         <p>Documentation</p>
-        <br>
+        <a href="javascript:void(0)" title="Linkedin"><i class="fa fa-linkedin fa-2x" style="padding: 0px 5px 0px 5px;color: #0077B5"></i></a>
+        <a href="javascript:void(0)" title="Linkedin"><i class="fa fa-github fa-2x" style="padding: 0px 5px 0px 5px;"></i></a>
+        <br><br>
       </div>
 
       <div class="w3-quarter">
@@ -167,7 +230,9 @@
         <!--<img src="/w3images/avatar.jpg" alt="Boss" style="width:45%" class="w3-circle w3-hover-opacity">-->
         <h4 style="margin: 10px">Michael Trexler</h4>
         <p>Documentation</p>
-        <br>
+        <a href="javascript:void(0)" title="Linkedin"><i class="fa fa-linkedin fa-2x" style="padding: 0px 5px 0px 5px;color: #0077B5"></i></a>
+        <a href="javascript:void(0)" title="Linkedin"><i class="fa fa-github fa-2x" style="padding: 0px 5px 0px 5px;"></i></a>
+        <br><br>
       </div>
   </div>
 </div>
@@ -183,15 +248,33 @@
 <!--Footer-->
   <footer class="footer-container">
     <h2 align="center">Follow the team on Social Media</h2>
-    <div id="social_icons" align="center" style="color: white; padding: 2px;;">
-      <a href="javascript:void(0)" title="Facebook"><i class="fa fa-facebook fa-4x" style="padding: 0px 5px 0px 5px;"></i></a>
-      <a href="javascript:void(0)" title="Twitter"><i class="fa fa-twitter fa-4x" style="padding: 0px 5px 0px 5px;"></i></a>
-      <a href="javascript:void(0)" title="Google +"><i class="fa fa-google-plus fa-4x" style="padding: 0px 5px 0px 5px;"></i></a>
-      <a href="javascript:void(0)" title="Google +"><i class="fa fa-instagram fa-4x" style="padding: 0px 5px 0px 5px;"></i></a>
-      <a href="javascript:void(0)" title="Linkedin"><i class="fa fa-linkedin fa-4x" style="padding: 0px 5px 0px 5px;"></i></a>
+    <div id="social_icons" align="center" style="color: white; padding: 10px; border-top: 1px solid #edeef0; border-bottom: 1px solid #edeef0;">
+      <a href="https://www.facebook.com/ExpenseMaster/" title="Facebook"><i class="fa fa-facebook fa-4x" style="padding: 0px 5px 0px 5px; color: #365899"></i></a>
+      <a href="https://twitter.com/Expense_Master" title="Twitter"><i class="fa fa-twitter fa-4x" style="padding: 0px 5px 0px 5px; color: #1da1f2"></i></a>
+      <a href="https://plus.google.com/106770097272433071838" title="Google +"><i class="fa fa-google-plus fa-4x" style="padding: 0px 5px 0px 5px; color: #d34836"></i></a>
+      <a href="https://www.linkedin.com/company-beta/16213827/" title="Linkedin"><i class="fa fa-linkedin fa-4x" style="padding: 0px 5px 0px 5px; color: #0077B5"></i></a>
     </div>
-    <br><br>
-    <h3 align="center" style=" color: white; font-size: 14px;line-height: 2">Designed by Stephen Weatherly ©2017</h3>
+    <div class="footer_links" style="color: white; display: inline-block; width: 100%;">
+          <div class="links1" style="float: left; width: 300px;">
+                <ul class="fa-ul fa-2x">
+                  <li style="font-size: .6em!important; font-weight: bold; padding-bottom: 3px;" >Contact Us</li>
+                  <li style="font-size: .6em!important;"><i class="fa-li fa fa-map-marker"></i>115 Library Dr, Rochester,MI</li>
+                  <li style="font-size: .6em!important;"><i class="fa-li fa fa fa-phone"></i>(586) XXX-XXXX</li>
+                  <li style="font-size: .6em!important;"><i class="fa-li fa fa-envelope"></i><a href="mailto:support@expense-master.com" style="text-decoration: none;">support@expense-master.com</a></li>
+              </ul>
+          </div>
+          <div class="links2" style="float: right; width: 300px;">
+              <ul class="fa-ul fa-2x" style="padding-top: 28px">
+                <li style="font-size: .6em!important;"><i class="fa-li fa fa fa-external-link" style="color: white"></i><a href="./login.php" style="text-decoration: none;">Login</a></li>
+                <li style="font-size: .6em!important;"><i class="fa-li fa fa fa-external-link" style="color: white"></i><a href="./forgotpwd.php" style="text-decoration: none;">Forgot Password</a></li>
+                <li style="font-size: .6em!important;"><i class="fa-li fa fa fa-external-link" style="color: white"></i><a href="#signup" style="text-decoration: none;">Sign Up</a></li>
+                <li style="font-size: .6em!important;"><i class="fa-li fa fa fa-external-link" style="color: white"></i><a href="#team" style="text-decoration: none;">About Us</a></li>
+              </ul>
+          </div>
+    </div>​
+    <div class ="footer_copyright">
+      <h3 align="center" style=" color: white; font-size: 14px;line-height: 2">Designed by Stephen Weatherly ©2017</h3>
+    </div>
   </footer>
   </body>
 </html>
